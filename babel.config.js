8d25591862104763b070c1b984afb3bfb8b5fef0
Doltons Edward Nicholas <75367447+doltonsedward@users.noreply.github.com>
2022-03-22 21:36:31 +0800

    feat: add plugin in babel.config.js
    - add alias and extension
    - add module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@core': './src/core',
+            '@lib': './src/libraries'
+          },
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json']
+        }
+      ]
+    ]
   };
 };
